9d0ccb56ad72c7b575b1707e00a26c4a20b21e96
hkim1016 <hankim0827@gmail.com>
2021-08-13 17:32:34 -0700

    Created the about and contact pages

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,24 @@ const app = express();
 
 const router = express.Router();
 
+// renders the homepage (not logged in)
 router.get('/', function(req, res) {
     res.render('../views/index');
 });
 
+// renders the about page
 router.get('/about', function(req, res) {
     res.render('../views/about');
+});
+
+// renders the contact page
+router.get('/contact', function(req, res) {
+    res.render('../views/contact');
+});
+
+// renders the main page (logged in)
+router.get('/tasks', function(req, res) {
+    res.render('../views/user/tasks');
 });
 
 module.exports = router;
