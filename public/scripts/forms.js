00732999cee9b94268fe4d43e53fac072e1a23e5
hkim1016 <hankim0827@gmail.com>
2021-08-16 00:19:30 -0700

    Checks if createTask inputs are filled and creates a calendar ui

---
+++ public/scripts/forms.js
@@ -0,0 +1,43 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {   
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+            form.classList.add('was-validated')
+        }, false)
+      })
+})()
+
+let dateClass='.date';
+$(document).ready(function ()
+{
+    if (document.querySelector(dateClass).type !== 'date')
+    {
+        var oCSS = document.createElement('link');
+        oCSS.type='text/css'; oCSS.rel='stylesheet';
+        oCSS.href='//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/base/jquery-ui.css';
+        oCSS.onload=function()
+    {
+        var oJS = document.createElement('script');
+        oJS.type='text/javascript';
+        oJS.src='//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js';
+        oJS.onload=function()
+        {
+            $(dateClass).datepicker();
+        }
+        document.body.appendChild(oJS);
+    }
+    document.body.appendChild(oCSS);
+    }
+});
