8948de7cda46f73506da487c1f8fd1157daf2cc8
hkim1016 <hankim0827@gmail.com>
2021-08-16 04:49:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const chalk = require('chalk')
+require('dotenv').config()
+
+const chalk = require('chalk');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+const MONGO_DB = process.env.MONGO_DB || 'mongodb+srv://hkim1016:Han135Kim1077@todolist.nbirn.mongodb.net/todolistDB';
 
 app.use(express.static(process.cwd() + '/public'));
 app.set('view engine', 'ejs'); 
+
+mongoose.connect(MONGO_DB, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', require('./routes'));
 app.use('/user', require('./routes/user'));
