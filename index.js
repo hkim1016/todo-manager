1651ea27eec5a1599ebe2468106fefa392067af5
hkim1016 <hankim0827@gmail.com>
2021-08-16 00:17:38 -0700

    Installed mongoose

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(express.static(process.cwd() + '/public'));
 app.set('view engine', 'ejs'); 
 
 app.use('/', require('./routes'));
+app.use('/user', require('./routes/user'));
+app.use('/task', require('./routes/task'));
 
 app.listen(PORT, function() {
     console.log(chalk.blue(`Server running at http://localhost:${PORT}/`));
