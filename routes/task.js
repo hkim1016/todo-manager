9d6f4eaae19d221b3ed86a25875a73fdfe386490
hkim1016 <hankim0827@gmail.com>
2021-08-16 05:01:17 -0700

    Created the routes and made route to create a new task

--- routes/task.js
+++ routes/task.js
@@ -8,7 +8,41 @@ const Task = require('../models/Task');
  
 /*
  TODO
+ - get: get all tasks
  - post: create new task
  - put: update selected task
  - delete: delete selected task
 */
+
+// Getting all tasks for the user
+router.get('/', (req, res) => {
+
+});
+
+// Creating a new task
+router.post('/', (req, res) => {
+    const task = new Task({
+        taskName: req.body.taskName,
+        taskSummary: req.body.taskSummary,
+        date: req.body.date,
+        description: req.body.description
+    });
+
+    task.save((err) => {
+        if(err) console.error(err);
+
+        res.redirect('/tasks');
+    });
+});
+
+// Updating a specific task
+router.put('/:id', (req, res) => {
+
+});
+
+// Deleting a specific task
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router;
