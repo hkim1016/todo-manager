5d3f3ee7efe58043b80f2f18178a1f3053c4ae4e
hkim1016 <hankim0827@gmail.com>
2021-08-19 18:05:40 -0700

    Added express-session

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ require('dotenv').config()
 
 const chalk = require('chalk');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const passport = require('passport');
 const express = require('express');
 const app = express();
 
@@ -20,13 +24,35 @@ app.use((req, res, next) => {
 
 app.use(methodOverride('_method'));
 
-mongoose.connect(MONGO_DB, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+mongoose.connect(MONGO_DB, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useFindAndModify: false,
+    useCreateIndex: true
+});
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({ 
+    secret: "00c683c952b404964b3e4f759ef205feaa52ec097755cabbe4068f96939e7394",
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    resave: false
+}));
+
+// app.use(cookieParser());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+    res.locals.user = req.session.user;
+    next();
+})
 
 app.use('/', require('./routes'));
 app.use('/user', require('./routes/user'));
