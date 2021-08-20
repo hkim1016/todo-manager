20a0fc3f074b7d5a5dd25607c93ceeea1cb9d818
hkim1016 <hankim0827@gmail.com>
2021-08-19 18:41:13 -0700

    Implemented 'confirm password' functionality

--- public/scripts/forms.js
+++ public/scripts/forms.js
@@ -41,3 +41,13 @@ $(document).ready(function ()
     document.body.appendChild(oCSS);
     }
 });
+
+function onChange() {
+    const password = document.querySelector('input[name=password]');
+    const confirm = document.querySelector('input[name=confirm]');
+    if (confirm.value === password.value) {
+        confirm.setCustomValidity('');
+    } else {
+        confirm.setCustomValidity('Passwords do not match');
+    }
+}
